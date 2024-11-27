594e9e76a0e2d6a4b3f4e60fd113da1c97031ca8
Khalid Hameed <khalidhameed@Mazhars-MacBook-Air.local>
2024-11-27 16:43:19 +0500

    Updated app.py for Azure deployment

--- app.py
+++ app.py
@@ -247,4 +247,5 @@ def handle_serial_extraction():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
